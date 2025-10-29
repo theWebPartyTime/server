e4d4ac9ab55b98553aacd4b8a94a56c2f779966a
gitmaxlla <user@email>
2025-10-29 22:37:28 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	router := gin.Default()
+	router.GET("/", func(context* gin.Context) {
+		context.JSON(200, gin.H{
+			"message": "Welcome to WebPartyTime!",	
+		})
+	})
+
+	router.Run()
+}
